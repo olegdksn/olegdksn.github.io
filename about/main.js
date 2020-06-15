2426c6d17f68780f5ac3fdf27546b9ea4e03580d
olegdksn <63336229+olegdksn@users.noreply.github.com>
2020-06-15 11:09:01 +0800

    Create main.js

---
+++ about/main.js
@@ -0,0 +1,9 @@
+// Openweather
+
+weatherUrl='api.openweathermap.org/data/2.5/weather'
+const wetterAPI='1763822c1f9595fbdaa793c7f0b863bb'
+
+// Foursquare
+
+const clientid='K3E4DIO2SISYMRXJL4STMTG2LKUV2JZ3AFDLN3ZWBOEHJAYR' ;
+const clientsecret='YZDTS4JITRCOBE1CZQWUZPBATA4IRYYHN5TEODEN2LOJ45N5' ;
